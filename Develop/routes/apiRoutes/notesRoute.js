cfd019cea93842ae25027518ebdf55207290d8aa
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-12-29 22:39:17 -0800

    created get api portion for notes

--- Develop/routes/apiRoutes/notesRoute.js
+++ Develop/routes/apiRoutes/notesRoute.js
@@ -2,7 +2,20 @@ const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
 const db = require('../../db');
+
+// Allows all notes to have a Unique ID
 const { v4: uuidv4 } = require('uuid');
+
+// GET Api notes 
+router.get('/notes', (req, res) => {
+    fs.writeFile('../../db', (err, data) => {
+        if (err) throw err;
+        let Data = JSON.parse(data);
+        res.json(Data);
+    });
+})
+
+
 
 
 
