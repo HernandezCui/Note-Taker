16f02b0feb94460fe752570462bf245c9e2ee54d
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-12-31 17:08:47 -0800

    Initial commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes/index.js');
+const htmlRoutes = require('./routes/htmlRoutes/index.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
